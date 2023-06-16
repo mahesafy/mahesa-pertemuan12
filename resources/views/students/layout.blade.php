4541fec773950139f8e04573a3b9759a3daaa758
Mahesafy <55169719+mahesafy@users.noreply.github.com>
2023-06-16 17:28:40 +0700

    ganti judul hehe

--- resources/views/students/layout.blade.php
+++ resources/views/students/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 9 CRUD School Application</title>
+    <title>Tugas Pertemuan 12</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
